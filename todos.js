55c67ca6d3bf5f35be415301317501ffbac1f9dc
KennyHansen <hansen.kenny101@gmail.com>
2017-02-01 09:32:15 -0700

    answered "YES" to the questions

--- todos.js
+++ todos.js
@@ -2,13 +2,26 @@ const vm = new Vue({
     el: '#root',
     data() {
         return {
-            todos: []
+            todos: [],
+            completed:[],
+            todo: ''
         }
     },
     methods: {
         submitTodo(todo) {
             this.todos.push(todo)
+            this.todo = ''
+        },
+        submitCompleted(todo){
+            var i = this.todos.indexOf(todo)
+            this.todos.splice(i, 1)
+            this.completed.push(todo)
+        },
+         removeTodo(todo){
+            var i = this.todos.indexOf(todo)
+            this.todos.splice(i, 1)
         }
+        
     }
 })
 
